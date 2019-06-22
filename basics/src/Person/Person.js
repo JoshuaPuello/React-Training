7cad92ba38ab57f437873f0737dcccc3bef4ce15
Joshua Puello <joshuapuello@outlook.com>
2019-06-22 06:26:10 -0500

    [JPP] - Removing Radium to enable CSS modules

--- basics/src/Person/Person.js
+++ basics/src/Person/Person.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import Radium from 'radium';
 import './Person.css'
 
 const person = (props) => {
 
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    }
-
     return (
-        <div className = "Person" style = { style }>
+        <div className = "Person">
         <p onClick = { props.click }>
             Hi, i'm a {props.name} and i'm {props.age} years old!
         </p>
@@ -22,4 +15,4 @@ const person = (props) => {
 )
 };
 
-export default Radium(person);
+export default person;
