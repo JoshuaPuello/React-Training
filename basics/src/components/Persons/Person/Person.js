6d55299268615bec387e7b572579dfd3ee2b9128
Joshua Puello <joshuapuello@outlook.com>
2019-06-26 06:45:45 -0500

    [JPP] - Using hoc in Person.js

--- basics/src/components/Persons/Person/Person.js
+++ basics/src/components/Persons/Person/Person.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+
 import classes from './Person.css'
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
     render() {
         return (
-            <div className = { classes.Person }>
+            <Aux>
                 <p onClick = { this.props.click }>
                     Hi, i'm a { this.props.name } and i'm { this.props.age } years old!
                 </p>
                 <p>{ this.props.children }</p>
 
                 <input type = "text" onChange = { this.props.changed } value = { this.props.name }/>
-            </div>
+            </Aux>
         )
     }
 }
