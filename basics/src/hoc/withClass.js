309964f42140b83eb6abf626123d7659edc06f54
Joshua Puello <joshuapuello@outlook.com>
2019-06-27 06:30:38 -0500

    [JPP] - Passing unknow props

--- basics/src/hoc/withClass.js
+++ basics/src/hoc/withClass.js
@@ -3,7 +3,7 @@ import React from 'react';
 const withClass = (WrappedComponent, classes) => {
     return props => (
         <div className = { classes }>
-            <WrappedComponent/>
+            <WrappedComponent {...props}/>
         </div>
     );
 }
